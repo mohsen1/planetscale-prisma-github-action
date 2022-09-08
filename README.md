64e30a32f656eb0ee57f01a807a1ffd6380b6aed
Mohsen Azimi <mohsen.azimi@airbnb.com>
2022-09-08 09:41:51 -0400

    Working off main now

--- README.md
+++ README.md
@@ -1,31 +1,63 @@
 # PlanetScale Prisma Github Action
 
-* Automatically create database branches and deploy request in Github pull requests. 
-* Mark Github Check as pending until PlanetScale deploy request is approved. (This requires PlanetScale to support Web Hooks. TBD)
-* Deploy databases to production when a pull request is merged.
-* Automatically delete database branches when a pull request is closed or merged.
+- Automatically create database branches and deploy request in Github pull requests.
+- Mark Github Check as pending until PlanetScale deploy request is approved. (This requires PlanetScale to support Web Hooks. TBD)
+- Deploy databases to production when a pull request is merged.
+- Automatically delete database branches when a pull request is closed or merged.
 
 ### Future Features
-* Vercel preview with the new database branch
 
+- Vercel preview with the new database branch
 
 ## Work in progress
 
-This is a work in progress. It's being done as a public repository so I can use free Github Action hours 
+This is a work in progress. It's being done as a public repository so I can use free Github Action hours
 
 ### To-do items
+
 - [x] Detect Prisma schema changes
 - [x] Create a PlanetScale branch with the PR Git branch name
-- [ ] Push the schema to the PlanetScale branch 
-- [ ] Create a Deploy Request on PlanetScale for the PlanetScale branch 
+- [ ] Push the schema to the PlanetScale branch
+- [ ] Create a Deploy Request on PlanetScale for the PlanetScale branch
 - [ ] Link the Deploy Request to the PR as a Check
-- [ ] Mark the Check as failed until Deploy Request is approved 
-- [ ] Make sure Vercel Preview URLs are using the new DB branch 
+- [ ] Mark the Check as failed until Deploy Request is approved
+- [ ] Make sure Vercel Preview URLs are using the new DB branch
 - [ ] Merge the DB branch on PlanetScale after Git branch is merged into the `main` branch
 
 ## How to use
 
+Use this Github Action with the following configuration. We are depending on [tj-actions/changed-files](https://github.com/marketplace/actions/changed-files) to detect if the Prisma schema has changed. Note that `fetch-depth: 0` in `actions/checkout` is required for this action to work.
+
+
+
+```yaml
+steps:
+  - uses: actions/checkout@v3
+    with:
+      fetch-depth: 0 # important: required for tj-actions/changed-files to work
+
+  - name: Get changed files
+    id: changed-files
+    uses: tj-actions/changed-files@v29.0.4
+
+
+  - uses: mohsen1/planetscale-prisma-github-action@main
+    # Only run this action if the Prisma schema has changed
+    if: contains(steps.changed-files.outputs.modified_files, 'schema/prisma.schema')
+    with:
+      # The name of the PlanetScale database to use
+      database: my-database
+      # The name of the PlanetScale organization to use
+      organization: my-organization
+      # The name of the PlanetScale service account to use
+      planetscale-service-token-id: my-service-token-id
+      # The PlanetScale API key to use
+      planetscale-service-token: ${{ secrets.PLANETSCALE_SERVICE_TOKEN }}
+```
+
+<!--
 ### Setup PlanetScale credentials
+
 You will need the following secrets in your Github repository:
 
 - `PLANETSCALE_SERVICE_TOKEN` - PlanetScale API service token
@@ -41,7 +73,6 @@ You will need to configure the following environment variables:
 #### Optional Environment Variables
 - `PLANETSCALE_MAIN_BRANCH_NAME` - PlanetScale database branch name (defaults to `main`)
 - `PLANETSCALE_BRANCH_PREFIX` - Prefix to use for PlanetScale database branches (defaults to `pull-request-`)
-- `PLANETSCALE_DEB_DOWNLOAD_URL` - URL to download the PlanetScale CLI (defaults to the latest release on Github)
 - `PRISMA_SCHEMA_FILE_PATH` - Path to the Prisma schema file (defaults to `prisma/schema.prisma`)
 - `PRISMA_DB_PUSH_COMMAND` - Command to run to push the Prisma schema to the PlanetScale database (defaults to `npx prisma db push`)
-
+ -->
---
+++ index.js
@@ -0,0 +1,151 @@
+#!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+// @ts-check
+
+// This script is meant to run by Github Actions.
+const core = require("@actions/core");
+const { execSync } = require("child_process");
+
+/**
+ * @typedef PlanetScaleBranch
+ * @property {string} name Name of the branch
+ * @property {string} created_at
+ * @property {string} updated_at
+ */
+
+/**
+ * @typedef PlanetScaleDeployRequestDeployment
+ * @property {string} state State of the deployment
+ * @property {boolean} deployable
+ * @property {string | null} started_at When the deployment started
+ * @property {string | null} finished_at When the deployment finished
+ * @property {string | null} queued_at When the deployment finished
+ * 
+/**
+ * @typedef PlanetScaleDeployRequest
+ * @property {string} branch Name of the branch
+ * @property {string} id ID of the deploy request
+ * @property {boolean} approved Whether the deploy request is approved
+ * @property {string} state "open" | "closed"
+ * @property {string} created_at Date string
+ * @property {string} updated_at Date string
+ * @property {string} closed_at Date string
+ * @property {PlanetScaleDeployRequestDeployment} deployment
+ */
+
+/**
+ * Poor man's PlanetScale API client
+ */
+class PlanetScale {
+  constructor() {
+    const {
+      DB_NAME,
+      PLANETSCALE_SERVICE_TOKEN,
+      PLANETSCALE_SERVICE_TOKEN_ID,
+      PLANETSCALE_ORG,
+    } = process.env;
+
+    if (
+      !DB_NAME ||
+      !PLANETSCALE_SERVICE_TOKEN ||
+      !PLANETSCALE_SERVICE_TOKEN_ID ||
+      !PLANETSCALE_ORG
+    ) {
+      throw new Error("Missing environment variables");
+    }
+
+    this.DB_NAME = DB_NAME;
+    this.PLANETSCALE_SERVICE_TOKEN = PLANETSCALE_SERVICE_TOKEN;
+    this.PLANETSCALE_SERVICE_TOKEN_ID = PLANETSCALE_SERVICE_TOKEN_ID;
+    this.PLANETSCALE_ORG = PLANETSCALE_ORG;
+  }
+
+  get args() {
+    return [
+      `--service-token-id ${this.PLANETSCALE_SERVICE_TOKEN_ID}`,
+      `--service-token ${this.PLANETSCALE_SERVICE_TOKEN}`,
+      `--org ${this.PLANETSCALE_ORG}`,
+      "--json",
+      "--no-colors",
+    ].join(" ");
+  }
+
+  /**
+   * @param {string} cmd
+   */
+  command(cmd) {
+    return execSync(`pscale ${cmd} ${this.args}`, { encoding: "utf8" });
+  }
+
+  /**
+   * @param {"list" | "create"} command
+   * @param {string=} branchName
+   */
+  branch(command, branchName) {
+    return this.command(
+      `branch ${command} ${this.DB_NAME} ${branchName ? branchName : ""}`
+    );
+  }
+
+  /**
+   * @param {"list" | "create"} command
+   * @param {string=} arg Number of deploy request or name of the branch
+   */
+  deployRequest(command, arg) {
+    return this.command(
+      `deploy-request ${command} ${this.DB_NAME} ${arg ? arg : ""}`
+    );
+  }
+}
+
+async function main() {
+  const {PLANETSCALE_BRANCH_PREFIX} = process.env;
+  let approvedDeployRequest = false;
+
+  const planetScale = new PlanetScale();
+
+  const gitBranch = execSync("git rev-parse --abbrev-ref HEAD")
+    .toString()
+    .trim();
+  const branchPrefix = PLANETSCALE_BRANCH_PREFIX || "pull-request-";
+  const branchName = `${branchPrefix}${gitBranch}`;
+
+  /** @type {PlanetScaleBranch[]} */
+  const existingBranches = JSON.parse(planetScale.branch("list"));
+
+  if (existingBranches.find(({ name }) => name === branchName)) {
+    core.debug(`Database branch "${branchName}" already exists`);
+  } else {
+    core.debug(`Creating a PlanetScale database branch named ${branchName}`);
+    planetScale.branch("create", branchName);
+  }
+
+  /** @type {PlanetScaleDeployRequest[]} */
+  const deployRequests = JSON.parse(planetScale.deployRequest("list"));
+
+  const openDeployRequest = deployRequests.find(
+    (deployRequest) =>
+      deployRequest.branch === branchName && deployRequest.state === "open"
+  );
+
+  if (openDeployRequest) {
+    core.debug(`Found an existing deploy request for ${branchName}`);
+
+    if (openDeployRequest.approved) {
+      core.debug("Deploy request is already approved");
+      approvedDeployRequest = true;
+    }
+  } else {
+    core.debug(`Creating a deploy request for ${branchName}`);
+    planetScale.deployRequest("create", branchName);
+  }
+
+  if (!approvedDeployRequest) {
+    throw new Error(`Database deploy request for "${branchName}" branch is not approved`);
+  }
+}
+
+main().catch((err) => {
+  throw err
+});
