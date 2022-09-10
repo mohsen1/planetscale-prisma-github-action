5ebb1980f0802fbfe9b5d278a4bb9416fdb586ba
Mohsen Azimi <mohsen.azimi@airbnb.com>
2022-09-10 18:05:02 -0400

    Refactor and update comment one more time

--- create/create.js
+++ create/create.js
@@ -6,43 +6,23 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const fs = require("fs");
-const { execSync } = require("child_process");
 
 const PlanetScale = require("../PlanetScale");
-const { branchName } = require("../util");
-
-function createCommentBody(
-  content = "Working...",
-  header = `<h4>PlanetScale deploy request</h4>`
-) {
-  const footer = `
-    <br>
-    <sub>
-      <a href="https://github.com/mohsen1/planetscale-prisma-github-action">
-        PlanetScale Prisma Github Action
-      </a>
-    </sub>
-    <!-- PLANETSCALE_PRISMA_GITHUB_ACTION_COMMENT -->
-    `;
-
-  const cleanContent = content.replace(
-    new RegExp(process.env.PLANETSCALE_SERVICE_TOKEN, "g"),
-    "pscale_tkn_***"
-  );
-  return `${header}${cleanContent}${footer}`;
-}
+const {
+  branchName,
+  createCommentBody,
+  getComment,
+  getOpenDeployRequest,
+  updateCommentFor,
+} = require("../util");
 
 async function main() {
   const { GITHUB_TOKEN, GITHUB_WORKSPACE, PLANETSCALE_ORG, DB_NAME } =
     process.env;
-  let approvedDeployRequest = false;
 
   if (!GITHUB_TOKEN) {
     throw new Error("GITHUB_TOKEN environment variable is not set");
   }
-
-  core.debug(`Running as ${github.context.actor}`);
-  core.debug(`whoami: ${execSync("whoami")}`);
 
   const octokit = github.getOctokit(GITHUB_TOKEN);
 
@@ -54,25 +34,7 @@ async function main() {
 
   core.debug(`Existing Github comments: ${JSON.stringify(comments, null, 2)}`);
 
-  let comment = comments.data.find(
-    (comment) =>
-      comment.user?.login === "github-actions[bot]" &&
-      comment.body?.includes("PLANETSCALE_PRISMA_GITHUB_ACTION_COMMENT")
-  );
-
-  if (!comment) {
-    core.info("No existing comment found, creating a new one");
-    comment = (
-      await octokit.rest.issues.createComment({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        issue_number: github.context.issue.number,
-        body: createCommentBody(
-          "Creating a new deploy request for this branch..."
-        ),
-      })
-    ).data;
-  }
+  const comment = await getComment();
 
   core.debug(`Changing directory to ${GITHUB_WORKSPACE}`);
 
@@ -141,32 +103,9 @@ async function main() {
 
   fs.writeFileSync("/tmp/planetscale-password-name", name);
 
-  /** @type {import("../types").PlanetScaleDeployRequest[]} */
-  const deployRequests = JSON.parse(planetScale.deployRequest("list"));
-  const branchDeployRequest = deployRequests.filter(
-    ({ branch }) => branch === branchName
-  );
+  const openDeployRequest = await getOpenDeployRequest(branchName);
 
-  let openDeployRequest = branchDeployRequest.find(
-    ({ state }) => state === "open"
-  );
-
-  if (!openDeployRequest) {
-    core.debug(`Creating a deploy request for ${branchName}`);
-    openDeployRequest = JSON.parse(
-      planetScale.deployRequest("create", branchName)
-    );
-    const url = `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/pull/${github.context.payload.pull_request?.number}`;
-    const planetScaleComment = `This deploy request was automatically created by schema changes in ${url}`;
-    planetScale.deployRequest(
-      "review",
-      `${openDeployRequest?.number} --comment "${planetScaleComment}"`
-    );
-  }
-
-  if (!openDeployRequest) {
-    throw new Error("Failed to create a deploy request");
-  }
+  await updateCommentFor(openDeployRequest);
 
   core.setOutput("deploy-request-number", openDeployRequest.number);
   core.setOutput("deploy-request-state", openDeployRequest.state);
@@ -174,37 +113,6 @@ async function main() {
     "deploy-request-approved",
     JSON.stringify(Boolean(openDeployRequest.approved))
   );
-
-  const deployRequestLink = `
-    <a href='https://app.planetscale.com/${PLANETSCALE_ORG}/${DB_NAME}/deploy-requests/${openDeployRequest.number}'>
-      Deploy request #${openDeployRequest.number}
-    </a> for 
-    <a href="https://app.planetscale.com/${PLANETSCALE_ORG}/${DB_NAME}/${branchName}">
-      <code>${branchName}</code>
-    </a> database branch`;
-
-  /** @type {import("../types").PlanetScaleDeployRequestDiff[]} */
-  const diffs = JSON.parse(
-    planetScale.deployRequest("diff", String(openDeployRequest.number))
-  );
-  const diffsBody = diffs
-    .map(
-      ({ name, raw }) =>
-        `<details><summary>Schema changes (${name})</summary>\n\n\n\`\`\`diff\n${raw}\`\`\`\n\n\n</details>`
-    )
-    .join("\n");
-
-  await octokit.rest.issues.updateComment({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    comment_id: comment.id,
-    body: createCommentBody(
-      (approvedDeployRequest
-        ? `<p>${deployRequestLink}<b> was approved</b></p>`
-        : `<p>Waiting for ${deployRequestLink} to be approved by a PlanetScale admin</p>`) +
-        diffsBody
-    ),
-  });
 }
 
 main().catch((err) => {
--- util.js
+++ util.js
@@ -1,4 +1,12 @@
-const { PLANETSCALE_BRANCH_PREFIX, GITHUB_HEAD_REF } = process.env;
+const github = require("@actions/github");
+const core = require("@actions/core");
+const { execSync } = require("child_process");
+const PlanetScale = require("./PlanetScale");
+
+// @ts-check
+
+const { PLANETSCALE_BRANCH_PREFIX, GITHUB_HEAD_REF, GITHUB_TOKEN } =
+  process.env;
 
 // branch name has to be alphanumeric and start with a letter
 const branchName = (PLANETSCALE_BRANCH_PREFIX + GITHUB_HEAD_REF).replace(
@@ -6,6 +14,159 @@ const branchName = (PLANETSCALE_BRANCH_PREFIX + GITHUB_HEAD_REF).replace(
   "-"
 );
 
+const octokit = github.getOctokit(GITHUB_TOKEN);
+
+const planetScale = new PlanetScale(async (error) => {
+  throw error;
+});
+
+/** @param {string} body */
+async function updateComment(body) {
+  const comment = await getComment();
+
+  await octokit.rest.issues.updateComment({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    comment_id: comment.id,
+    body: createCommentBody(body),
+  });
+}
+
+/**
+ * // TODO: fix types
+ * @returns {Promise<{id: number}>}
+ */
+async function getComment() {
+  const comments = await octokit.rest.issues.listComments({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: github.context.issue.number,
+  });
+
+  core.debug(`Existing Github comments: ${JSON.stringify(comments, null, 2)}`);
+
+  let comment = comments.data.find(
+    (comment) =>
+      comment.user?.login === "github-actions[bot]" &&
+      comment.body?.includes("PLANETSCALE_PRISMA_GITHUB_ACTION_COMMENT")
+  );
+
+  if (!comment) {
+    core.info("No existing comment found, creating a new one");
+    comment = (
+      await octokit.rest.issues.createComment({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        issue_number: github.context.issue.number,
+        body: createCommentBody(
+          "Creating a new deploy request for this branch..."
+        ),
+      })
+    ).data;
+  }
+  return comment;
+}
+
+function createCommentBody(
+  content = "Working...",
+  header = `<h4>PlanetScale deploy request</h4>`
+) {
+  const footer = `
+    <br>
+    <sub>
+      <a href="https://github.com/mohsen1/planetscale-prisma-github-action">
+        PlanetScale Prisma Github Action
+      </a>
+    </sub>
+    <!-- PLANETSCALE_PRISMA_GITHUB_ACTION_COMMENT -->
+    `;
+
+  const cleanContent = content.replace(
+    new RegExp(process.env.PLANETSCALE_SERVICE_TOKEN, "g"),
+    "pscale_tkn_***"
+  );
+  return `${header}${cleanContent}${footer}`;
+}
+
+/**
+ *
+ * @param {string} branchName
+ */
+async function getOpenDeployRequest(branchName) {
+  /** @type {import("./types").PlanetScaleDeployRequest[]} */
+  const deployRequests = JSON.parse(planetScale.deployRequest("list"));
+  const branchDeployRequest = deployRequests.filter(
+    ({ branch }) => branch === branchName
+  );
+
+  let openDeployRequest = branchDeployRequest.find(
+    ({ state }) => state === "open"
+  );
+
+  if (!openDeployRequest) {
+    core.debug(`Creating a deploy request for ${branchName}`);
+    openDeployRequest = JSON.parse(
+      planetScale.deployRequest("create", branchName)
+    );
+    const url = `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/pull/${github.context.payload.pull_request?.number}`;
+    const planetScaleComment = `This deploy request was automatically created by schema changes in ${url}`;
+    planetScale.deployRequest(
+      "review",
+      `${openDeployRequest?.number} --comment "${planetScaleComment}"`
+    );
+  }
+
+  if (!openDeployRequest) {
+    throw new Error("Failed to create a deploy request");
+  }
+
+  return openDeployRequest;
+}
+
+/**
+ *
+ * @param {import("./types").PlanetScaleDeployRequest} openDeployRequest
+ */
+async function updateCommentFor(openDeployRequest) {
+  const comment = await getComment();
+
+  const deployRequestLink = `
+    <a href='https://app.planetscale.com/${PLANETSCALE_ORG}/${DB_NAME}/deploy-requests/${openDeployRequest.number}'>
+      Deploy request #${openDeployRequest.number}
+    </a> for 
+    <a href="https://app.planetscale.com/${PLANETSCALE_ORG}/${DB_NAME}/${branchName}">
+      <code>${branchName}</code>
+    </a> database branch`;
+
+  /** @type {import("./types").PlanetScaleDeployRequestDiff[]} */
+  const diffs = JSON.parse(
+    planetScale.deployRequest("diff", String(openDeployRequest.number))
+  );
+  const diffsBody = diffs
+    .map(
+      ({ name, raw }) =>
+        `<details><summary>Schema changes (${name})</summary>\n\n\n\`\`\`diff\n${raw}\`\`\`\n\n\n</details>`
+    )
+    .join("\n");
+
+  await octokit.rest.issues.updateComment({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    comment_id: comment.id,
+    body: createCommentBody(
+      (openDeployRequest.approved
+        ? `<p>${deployRequestLink}<b> was approved</b></p>`
+        : `<p>Waiting for ${deployRequestLink} to be approved by a PlanetScale admin</p>`) +
+        diffsBody
+    ),
+  });
+}
+
 module.exports = {
+  getComment,
+  createCommentBody,
   branchName,
+  updateComment,
+  getOpenDeployRequest,
+  updateCommentFor,
 };
